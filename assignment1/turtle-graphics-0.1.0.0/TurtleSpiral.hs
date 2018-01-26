6022a9ee6d019fbf8c3b378fd78d2fa32b24844b
Tobias Rastemo <tobias.rastemo@gmail.com>
2018-01-26 13:37:33 +0100

    part 1 task 3, spiral program

---
+++ assignment1/turtle-graphics-0.1.0.0/TurtleSpiral.hs
@@ -0,0 +1,20 @@
+module TurtleSpiral where
+
+import Turtle1
+
+-- | A finite spiral turning d radians each step and stopping at 100 steps
+spiralFin :: Double -> Double -> Program
+spiralFin s d | s > 100   = die
+              | otherwise = forward s >*> right d >*> spiralFin (s + 2) d
+
+-- | An infinite spiral turning d radians each step
+spiralInf :: Double -> Double -> Program
+spiralInf s d = forward s >*> right d >*> spiralInf (s + 2) d
+
+-- | An alternate definition of spiralFin using spiralInf
+spiralFin' :: Double -> Double -> Program
+spiralFin' s d = lifespan (102 - s `div` 2) $ spiralInf s d
+
+-- | An infinite spiral program starting with a finite spiral and 
+spiralFinInf :: Double -> Double -> Program
+spiralFinInf s d = spiralFin s d >*> spiralInf 102 d
