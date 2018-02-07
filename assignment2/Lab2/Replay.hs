9be5c8f11818993c75a240b9b07a51b73bdf68f2
Tobias Rastemo <tobias.rastemo@gmail.com>
2018-02-07 11:23:56 +0100

    continoued implementation, not complete

--- assignment2/Lab2/Replay.hs
+++ assignment2/Lab2/Replay.hs
@@ -1,67 +1,85 @@
 {-# Language GADTs #-}
-module Replay where
 
+module Replay(
+  -- * Types
+  Replay, Trace, Item
+  -- * Primitive
+  , io
+  , ask
+  , addAnswer
+  , emptyTrace
+  -- * Derived
+  
+  -- * Run
+  , run
+  ) where
 
--- Types
+import Control.Monad (liftM,ap)
+
+-- * Types
+--------------------------------------------------------------------------------
 
 data Replay q r a where
     -- Constructors
-    Ask         :: q -> Replay q r a
-    InOu        :: IO a -> Replay q r a
-    Return      :: Replay q r a
+    Ask    :: q -> Replay q r r
+    InOut  ::(Show a, Read a) => IO a -> Replay q r a
+    Return :: a -> Replay q r a
     -- Combinators 
-    Bind        :: Replay q r a -> (a -> Replay q r b) -> Replay q r b
-
-    -- What?! 
-    Left        :: (q, Trace r) -> Replay q r a
-    Right       :: x -> Replay q r a
-    -- Run function
-    Run         :: Replay q r a -> Trace r -> IO (Either (q, Trace r) a)
-
+    Bind   :: (Replay q r) a -> (a -> (Replay q r) b) -> (Replay q r) b
 
 -- | Adding the QA r first in the list.
-type Trace r = [QA r] 
+type Trace r = [Item r] 
 
-data QA r = Q r | A r 
+data Item r = Answer r | Result String
+  deriving (Show,Read)
 
-
-instance Show (QA r) where 
-    show (Q r) = show r ++ "?"
-    show (A r) = show r ++ "."
-
-
-emptyTrace :: Trace r
-emptyTrace = []
-
-
--- Operations
+-- * Operations
+--------------------------------------------------------------------------------
 instance Monad (Replay q r) where 
     return = Return
-    (>>=) = Bind
+    (>>=)  = Bind
 
--- instance Show r => Show (Trace r)
--- instance Read r => Read (Trace r)
-
-
-io  :: (Show a, Read a) => IO a -> Replay q r a
-io a = InOu a
+io :: (Show a, Read a) => IO a -> Replay q r a
+io a = InOut a
 
 ask :: q -> Replay q r r
 ask q = Ask q
 
-addAnswer  :: Trace r -> r -> Trace r
-addAnswer t r = r ++ t
+-- * Trace manipulation
+--------------------------------------------------------------------------------
+
+emptyTrace :: Trace r
+emptyTrace = []
+
+addAnswer :: Trace r -> r -> Trace r
+addAnswer t r = Answer r : t
+
+-- * Run function
+--------------------------------------------------------------------------------
+
+-- Ask    :: q -> Replay q r a
+-- InOut   :: IO a -> Replay q r a
+-- Return :: a -> Replay q r a
+-- Bind   :: Replay q r a -> (a -> Replay q r b) -> Replay q r b
 
 run :: Replay q r a -> Trace r -> IO (Either (q, Trace r) a)
-run = undefined
+run (Ask q)    t      = return $ Left (q,t)
+run (InOut io) []     = do
+  a <- io
+  return temp
+  where temp = undefined
+ 
+run (InOut io) (i:is) = undefined
+  
+run (Return a) t = return (Right a)
+run (Bind a f) t = undefined
+
+-- * Monad magic
+--------------------------------------------------------------------------------
 
 instance Functor (Replay q r) where
     fmap = liftM
 
 instance Applicative (Replay q r) where
-    pure           = Just
-    Nothing <*> vv = Nothing
-    Just f  <*> vv = fmap f vv 
-
-
-
+    pure  = return
+    (<*>) = ap
