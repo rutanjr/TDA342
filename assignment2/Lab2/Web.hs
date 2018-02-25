ad6b2cf5fc8e7b366fc898a23c2bf4b338b6e3a1
Tobias Rastemo <test@test.com>
2018-02-25 11:10:07 +0100

    stuff

--- assignment2/Lab2/Web.hs
+++ assignment2/Lab2/Web.hs
@@ -12,6 +12,8 @@ import Web.Scotty     (ActionM, ScottyM, scotty, get, post, put, rescue, html, p
 import Data.Text.Lazy (Text, unpack, pack)
 import Replay
 import Control.Monad.IO.Class (liftIO)
+import Codec.Binary.Base64.String
+import Data.Text.Encoding
 
 -- * Data types
 --------------------------------------------------------------------------------
@@ -22,35 +24,57 @@ type Test a = Replay Int (Int -> Int) a
 type Form = [String]
 type Answers = [String]
 
-mkForm :: Form
-mkForm = undefined
-
 -- * Run
 --------------------------------------------------------------------------------
 
 runWeb :: Web a -> ActionM ()
-runWeb webProg = web emptyTrace
-  where
-    web :: Trace Answers -> ActionM ()
-    web t = do -- action land
-      r <- liftIO $ run webProg t
-      case r of
-        Left (q,t') -> do -- action land
-          html $ fromForm q
-          rescue
-            (do
-                is <- sequence $ map param $ take (length q) $ inputnames
-                web $ addAnswer t' (map unpack is))
-            (\_ -> web t')
-        Right x     -> return ()
+runWeb webProg = do -- action land  
+  t <- getTrace
+  ta <- getAnswers t
+  temp <- params
+  liftIO $ putStrLn (show temp)
+  r <- liftIO $ run webProg ta
+  case r of
+    Left (q,t') -> do -- action land
+      html (fromForm t' q)
+    r -> liftIO (putStrLn "OK") --return ()
+  where    
+    getTrace :: ActionM (Trace Answers)
+    getTrace = rescue (do
+                          t <- param "trace"
+                          return . read . decode $ t                          
+                      )
+                      (\_ -> return emptyTrace)
 
+    getAnswers :: Trace Answers -> ActionM (Trace Answers)
+    getAnswers _ = do
+      input <- params
+      let (is,[t]) = break (("trace" ==) . fst) input
+      undefined
 
-fromForm :: Form -> Text
-fromForm f = mconcat $
+      -- rescue (param "input1" >>= \i -> return (addAnswer t [i]))
+      --                     (\_ -> return t)
+
+running :: Replay String String a -> IO a
+running prog = play emptyTrace
+ where
+  play t = do
+    r <- run prog t    -- this is the same prog every time!
+    case r of
+      Left (q,t2) -> do
+        putStr ("Question: " ++ q ++ " ")
+        r <- getLine
+        play (addAnswer t2 r)
+      Right x -> return x                   
+
+fromForm :: Trace Answers -> Form -> Text
+fromForm t f = mconcat $
   [ "<html><body>"
   , "<form method=post>"]
   ++ textFields ++
   [ "<input type=submit value=OK>"
+  , "<input type=hidden name=trace value="
+  , pack $ (concat . lines . encode . show) t ++ ">"
   , "</form>"
   , "</body></html>"
   ]
@@ -68,14 +92,13 @@ inputnames = map (\n -> pack $ "input" ++ show n) [1..]
 
 main :: IO ()
 main = scotty 3000 $ do
-  get "/" $ runWeb example
+  get  "/" $ runWeb example
   post "/" $ runWeb example
-
+  
 example :: Web ()
 example = do
-  s <- ask ["String here", "and here"]
-  io $ sequence $ map putStrLn s
-  return ()
-
-ageForm :: Form
-ageForm = undefined
+  a1 <- ask ["Name?"]
+  [s1,s2] <- ask ["Age?","Yes"]
+  if (read s1 < 18)
+    then (ask ["Must be over 18"] >> example)
+    else return ()
