d7fca82e9eb4ef7da693055289efb3b5a0bb8811
Oskar Rutqvist <rutanjr@gmail.com>
2018-02-25 20:42:08 +0100

    some comments etc.

--- assignment2/Lab2/Web.hs
+++ assignment2/Lab2/Web.hs
@@ -8,7 +8,8 @@ module Web (
   
   ) where
 
-import Web.Scotty     (ActionM, ScottyM, scotty, get, post, put, rescue, html, param, params, text)
+import Web.Scotty     (ActionM, ScottyM, scotty, get, post, 
+  put, rescue, html, param, params, text)
 import Data.Text.Lazy (Text, unpack, pack)
 import Replay
 import Control.Monad.IO.Class (liftIO)
@@ -17,15 +18,22 @@ import Codec.Binary.Base64.String
 -- * Data types
 --------------------------------------------------------------------------------
 
+-- | Just a shorthand for an instance of the replay monad.
 type Web a = Replay Form Answers a
 type Test a = Replay Int (Int -> Int) a
 
+-- | Our form that has two parts, first the header as a String, and then
+-- all of our questions as a list of strings.
 type Form = (String,[String])
+
+-- | Answers is just a list of strings, since that is what our input is.
 type Answers = [String]
 
 -- * Run
 --------------------------------------------------------------------------------
 
+-- | Runs our Web program. Extracts data and updates the web page (html) 
+-- according to the programs wishes.
 runWeb :: Web a -> ActionM ()
 runWeb webProg = do -- action land  
   t <- getTrace
@@ -38,13 +46,14 @@ runWeb webProg = do -- action land
       html (fromForm t' q)
     r -> liftIO (putStrLn "OK") --return ()
   where    
+    -- Gets the trace from the html page.
     getTrace :: ActionM (Trace Answers)
     getTrace = rescue (do
                           t <- param "trace"
                           return . read . decode $ t                          
                       )
                       (\_ -> return emptyTrace)
-
+    -- Help function that takes all input from the page except the trace.
     getAnswers :: Trace Answers -> ActionM (Trace Answers)
     getAnswers t = do
       input <- params
@@ -53,24 +62,14 @@ runWeb webProg = do -- action land
         [] -> return t
         _  -> return $ addAnswer t $ map (unpack . snd) is
 
-running :: Replay String String a -> IO a
-running prog = play emptyTrace
- where
-  play t = do
-    r <- run prog t    -- this is the same prog every time!
-    case r of
-      Left (q,t2) -> do
-        putStr ("Question: " ++ q ++ " ")
-        r <- getLine
-        play (addAnswer t2 r)
-      Right x -> return x                   
-
+-- | Takes our trace, a form, and creates our html page.
 fromForm :: Trace Answers -> Form -> Text
 fromForm t (title,f) = pack $ mconcat $
-  [ "<html><body>"
+  [ "<html>"
+  , "<head><title>" ++ title ++ "</title><h2>" ++ title ++ "</h2></head>"
+  , "<body>"
   , "<form method=post>"
 --  , "<p>" ++ title ++ "</p>"
-  , title
   , textFields
   , "<input type=submit value=OK>"
   , "<input type=hidden name=trace value="
@@ -80,19 +79,25 @@ fromForm t (title,f) = pack $ mconcat $
   ]
   where
     textFields = concat $ zipWith textField f (map unpack inputNames)
+    textField :: String -> String -> String
     textField t l = "<p>" ++ t ++ "</p><input name=" ++ l ++ ">"
       
+-- Just gives a list of names for our inputboxes, input1, input2, input3,... 
 inputNames :: [Text]
 inputNames = map (\n -> pack $ "input" ++ show n) [1..]
 
 -- * Example
 --------------------------------------------------------------------------------
 
+-- | Basically only runs our web program (starts scotty at port 3000 
+-- and runs the example program)
 main :: IO ()
 main = scotty 3000 $ do
   get  "/" $ runWeb example
   post "/" $ runWeb example
   
+
+-- | Our exciting example program
 example :: Web ()
 example = do
   a1 <- ask ("Titletest",["Name?"])
